3ace95a689cf23bf275c84fa63c7110ee03fe182
Nirmal Kumar <nirmal1064@gmail.com>
2022-04-15 21:25:43 +0530

    separate module to export default state values

---
+++ utils/index.ts
@@ -0,0 +1,22 @@
+export const resultDefault = {
+  season: 2022,
+  matchNumber: 0,
+  teamA: 0,
+  teamB: 0,
+  scoreA: 0,
+  scoreB: 0,
+  oversA: 0,
+  oversB: 0,
+  maxOversA: 0,
+  maxOversB: 0,
+  allOutA: false,
+  allOutB: false,
+  oversAFinal: 0,
+  oversBFinal: 0,
+  ballsAFinal: 0,
+  ballsBFinal: 0,
+  winner: 0,
+  dlApplied: false,
+  noResult: false,
+  tie: false
+};
